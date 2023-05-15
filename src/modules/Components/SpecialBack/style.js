a3eb6f54b48fb28a82df8746bc5e031c4002bb67
Venia <dev14@globalpartnersoft.com>
2023-05-15 21:20:03 +0300

    background for 1 block

--- src/modules/Components/SpecialBack/style.js
+++ src/modules/Components/SpecialBack/style.js
@@ -6,9 +6,16 @@ export const Wrap = styled.div`
 `;
 
 export const Img = styled.img`
-  height: 30vh;
   width: 100vw;
+  height: 50vh;
   object-fit: cover;
+
+  @media screen and (max-width: 768px) {
+    height: 30vh;
+  }
+  @media screen and (max-width: 480px) {
+    height: fit-content;
+  }
 `;
 
 export const ReverseImg = styled(Img)`
