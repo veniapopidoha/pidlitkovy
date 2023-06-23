1a99d747e09aaad3552e507e0982b4461942bd8a
Venia <dev14@globalpartnersoft.com>
2023-06-23 21:30:51 +0300

    payment

--- src/modules/Support/index.js
+++ src/modules/Support/index.js
@@ -32,7 +32,7 @@ export const Support = (props) => {
       <Button
         data-aos-duration='1000'
         data-aos='fade-up'
-        href='https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiIxMDAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoi0JTQvtCx0YDQvtCy0ZbQu9GM0L3RliDQv9C+0LbQtdGA0YLQstGD0LLQsNC90L3RjyIsInB1YmxpY19rZXkiOiJzYW5kYm94X2kzODAxNTQ0NTgwNSIsImxhbmd1YWdlIjoidWsifQ==&signature=SqcdpYo2lXkeM3XkODb8qsAJLHM='
+        href='https://www.liqpay.ua/en/checkout/i9435223620'
       >
         Пожертвувати
       </Button>
